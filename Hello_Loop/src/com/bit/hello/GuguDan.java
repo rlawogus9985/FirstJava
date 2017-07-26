fbc645c45852bd991a4e43caef8c7db9b1d069d1
rlawogus9985 <rlawogus9985@naver.com>
2017-07-26 16:10:13 +0900

    first commit

---
+++ Hello_Loop/src/com/bit/hello/GuguDan.java
@@ -0,0 +1,21 @@
+package com.bit.hello;
+
+public class GuguDan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("============================");
+		System.out.println("   5단   구   구   단 ");
+		System.out.println("============================");
+		
+		for(int i = 1 ; i < 10; i++) {
+			System.out.println("5 x " + i + "=" + (5*i));
+		}
+		
+		
+		
+		
+	}
+
+}
