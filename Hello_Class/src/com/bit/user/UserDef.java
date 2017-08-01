b5d0ddf4df09401dd40c35e058a574a7995a6179
rlawogus9985 <rlawogus9985@naver.com>
2017-08-01 17:28:36 +0900

    2017.8.1

---
+++ Hello_Class/src/com/bit/user/UserDef.java
@@ -0,0 +1,16 @@
+package com.bit.user;
+
+
+// UserDef라는 이름의 클래스를 디자인(선언)
+public class UserDef {
+
+	// exec라는 이름의 method를 디자인(선언)
+	public void exec() {
+		
+		// exec method 내에서 사용되는 일반 변수. member변수 x
+		int intAge = 10;
+		
+		System.out.println("UserDef의 exec Method");
+	}
+	
+}
