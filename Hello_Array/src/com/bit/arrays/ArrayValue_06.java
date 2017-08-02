5ceade9bccc5dd75693649ee99fc231bb785d472
rlawogus9985 <rlawogus9985@naver.com>
2017-08-02 17:48:00 +0900

    2017.8.2

---
+++ Hello_Array/src/com/bit/arrays/ArrayValue_06.java
@@ -0,0 +1,24 @@
+package com.bit.arrays;
+
+public class ArrayValue_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * strNation[0] = "대한민국";
+		 *  strNation[1] = "러시아";
+		 *   strNation[2] = "중국";
+		 * strNation[3] = "미연방";
+		 *  strNation[4] = "일본";
+		 */
+
+		String[] strNation = new String[] {
+
+				"대한민국", "러시아", "중국", "미연방", "일본"
+
+		}; // 여기는 } 다음에 반드시 ; 이 와야한다
+		   // 배열의 개수가 적을때는 쓸수도 있지만 별로 좋은 코드는 아니다.
+
+	}
+
+}
